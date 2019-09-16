6a05912507dbadaaad9c6932cbbef1b870c438d5
Teva Henry <35941084+TevaHenry@users.noreply.github.com>
2019-09-15 21:30:25 -1000

    Updates ProjectRecruitment component

--- src/components/Pages/ProjectDetails/ProjectRecruitment/ProjectRecruitment.jsx
+++ src/components/Pages/ProjectDetails/ProjectRecruitment/ProjectRecruitment.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import './ProjectRecruitment.css';
 
-const ProjectRecruitment = ({ developers = 2, designers = 1, devops = 1 }) => (
+const ProjectRecruitment = ({ recruits: { developer, designer, operator }}) => (
   <div className='project-recruitment'>
     <h3>This project is recruiting!</h3>
     <div className="recruitment-roles">
       <div className="developer-role">
-        Developer x {developers}
+        Developer x {developer}
       </div>
       <div className="designer-role">
-        Designer x {designers}
+        Designer x {designer}
       </div>
       <div className="devops-role">
-        DevOps x {devops}
+        DevOps x {operator}
       </div>
     </div>
     <button className="apply-button">Apply Now!</button>
